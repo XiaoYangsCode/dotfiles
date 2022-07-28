7be0d310702821e668b82f19ad088541000321d4
Yangtianming <2415180498@qq.com>
2022-07-28 11:20:33 +0800

    fix md folder bug

--- .config/nvim/lua/plugin-config/vim-markdown.lua
+++ .config/nvim/lua/plugin-config/vim-markdown.lua
@@ -1,9 +1,7 @@
 vim.cmd(
-  [[
+    [[
       let g:vim_markdown_folding_disabled = 0
       let g:vim_markdown_math = 1
+      let g:vim_markdown_no_default_key_mappings = 1
   ]]
 )
-
---   let g:vim_markdown_no_default_key_mappings = 1
-      
