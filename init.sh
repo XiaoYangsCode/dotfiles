5489eb6689898c17f9f951b48fbf74e2ba5b59d7
XiaoYang <2415180498@qq.com>
2022-08-28 13:25:40 +0800

    add init shell

---
+++ init.sh
@@ -0,0 +1,26 @@
+mkdir ~/.config
+
+echo "start remove old link"
+rm .zshrc
+rm .clang-format
+rm -rf ~/.config/alacritty
+rm -rf ~/.config/kitty
+rm -rf ~/.config/nvim
+rm -rf ~/.config/ranger
+rm -rf ~/.config/swaylock
+rm -rf ~/.config/waybar
+rm -rf ~/.config/wofi
+rm ~/.config/wayfire.ini
+
+echo "start create new link"
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.clang-format ~/.clang-format
+ln -s ~/dotfiles/.config/alacritty ~/.config/alacritty
+ln -s ~/dotfiles/.config/kitty ~/.config/kitty
+ln -s ~/dotfiles/.config/nvim ~/.config/nvim
+ln -s ~/dotfiles/.config/ranger ~/.config/ranger
+ln -s ~/dotfiles/.config/swaylock ~/.config/swaylock
+ln -s ~/dotfiles/.config/waybar ~/.config/waybar
+ln -s ~/dotfiles/.config/wofi ~/.config/wofi
+ln -s ~/dotfiles/.config/wayfire.ini ~/.config/wayfire.ini
+
